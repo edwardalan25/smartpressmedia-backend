57883a528254d8855b6adceea22df363eb673e4e
ASADAMJAD922 <asadamjad922@gmail.com>
2025-12-20 21:41:54 +0500

    create cart items model and migration

---
+++ app/Models/CartItem.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    protected $fillable = [
+        'cart_id',
+        'product_id',
+        'product_variant_id',
+        'quantity',
+        'price',
+        'total_price'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
