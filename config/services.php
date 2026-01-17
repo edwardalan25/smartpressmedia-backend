2dfa2835cdd6474ccb08bc4c12e2dd5cd76f923c
ASADAMJAD922 <asadamjad922@gmail.com>
2026-01-17 18:49:04 +0500

    stripe configuration

--- config/services.php
+++ config/services.php
@@ -35,4 +35,12 @@ return [
         ],
     ],
 
+    'stripe' => [
+        'secret' => env('STRIPE_SECRET'),
+        'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
+        'currency' => env('STRIPE_CURRENCY', 'usd'),
+        'success_url' => env('STRIPE_SUCCESS_URL'),
+        'cancel_url' => env('STRIPE_CANCEL_URL'),
+    ],
+
 ];
